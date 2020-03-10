4a077432f4d124cce3eb3d76af812064659c869a
Maico Williams <williamsmaico@gmail.com>
2020-03-10 19:34:10 -0300

    Matrix class for dot product (scalar)

---
+++ NeuralNetwork/Utilities/Matrix.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetwork.Utilities
+{
+    internal static class Matrix
+    {
+        public static double[,] DotProduct(double[,] matrixA, double[,] matrixB)
+        {
+            var product = new double[matrixA.GetLength(0), matrixB.GetLength(1)];
+            if(IsGeoEquivalent(matrixA, matrixB))
+            {
+                for(int i = 0; i < matrixA.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrixB.GetLength(1); j++)
+                    {
+                        double sum = 0;
+                        for (int k = 0; k < matrixA.GetLength(1); k++)
+                        {
+                            sum += matrixA[i, k] * matrixB[k,j];
+                        }
+                        product[i, j] = sum;
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("The matrices are not geo-equivalent.");
+            }
+
+            return product;
+        }
+
+        private static bool IsGeoEquivalent(double[,] matrixA, double[,] matrixB)
+        {
+            return matrixA.GetLength(1) == matrixB.GetLength(0) ? true : false;
+        }
+    }
+}
