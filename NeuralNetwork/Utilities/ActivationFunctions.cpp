71b144c42270408bc7a3ee825d4f025699ce7248
Maico Williams <williamsmaico@gmail.com>
2020-03-13 19:48:00 -0300

    Updating the sigmoid function

--- NeuralNetwork/Utilities/ActivationFunctions.cpp
+++ NeuralNetwork/Utilities/ActivationFunctions.cpp
@@ -5,9 +5,6 @@
 #include "framework.h"
 #include "ActivationFunctions.h"
 
-#define M_2_SQRTPI    1.12837916709551257390    /* 2/sqrt(pi) */
-#define ERF_COEF      (1.0/M_2_SQRTPI)
-
 float sigmoid(const float x) {
-    return erf(ERF_COEF * x);
+    return 1.0 / (1.0 + exp(-x));
 }
